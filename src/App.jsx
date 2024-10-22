db41157986ab446c145587d8d275fbe2d822bb94
Xiaole Su <suxiaole0223@gmail.com>
2024-10-21 23:59:27 -0400

    feat: added libraries and setup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <p>{import.meta.env.GOOGLE_CLIENT_ID}</p>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Router>
+        </GoogleOAuthProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+  );
 }
 
 export default App
