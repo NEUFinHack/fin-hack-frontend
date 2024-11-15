4ed4a875d11c9be86e4247453156ddcf2e15ab4d
Xiaole Su <suxiaole0223@gmail.com>
2024-11-15 16:04:34 -0500

    code

---
+++ src/api/axios.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Base URL from .env file
+  headers: {
+    'Content-Type': 'application/json', // Default header for JSON requests
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken'); // Assuming token is stored in localStorage
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// axiosInstance.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//       if (error.response && error.response.status === 401) {
+//         console.error('Token expired or invalid. Redirecting to login...');
+//         localStorage.removeItem('authToken'); 
+//         window.location.href = '/login';
+//       }
+//       return Promise.reject(error); 
+//     }
+//   );
+
+// export default axiosInstance;
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),svgr()],
+  server : {
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
 
