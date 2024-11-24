e0781c55248635c8eeb3751100f9dabfb2856d0a
QW66666 <wusteven0829@gmail.com>
2024-11-24 02:26:30 -0500

    added background image to homepage

--- src/components/Pages/Title.jsx
+++ src/components/Pages/Title.jsx
@@ -34,7 +34,7 @@ export default function Title() {
 
   return (
     <div className="min-h-screen bg-[#0a1628]">
-      <div className="bg-cover bg-center  w-screen min-h-screen bg-no-repeat"  style={{ backgroundImage: `url(src/assets/background.png) `}}>
+      <div className="bg-cover bg-center  w-screen min-h-screen bg-no-repeat"  style={{ backgroundImage: '/assets/background.png'}}>
 
       {/* Navigation */}
       <Navbar/>
